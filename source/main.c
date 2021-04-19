b8bb6aca50121576d74d6de383c6e35f9b981c51
CarlosLoeza <loeza.carlos@yahoo.com>
2021-04-18 21:45:20 -0700

    Completed Part 1

--- source/main.c
+++ source/main.c
@@ -26,6 +26,7 @@ void Blink(){
 	case TurnOn_PB0:
 	    if(tmpA){
 		TurnOn_State = TurnOn_PB0_Wait;
+	  	tmpB = 0x02;
 	    }else if(!tmpA) {
 		TurnOn_State = TurnOn_PB0;
 	    }
@@ -40,6 +41,7 @@ void Blink(){
 	case TurnOn_PB1:
 	    if(tmpA){
 		TurnOn_State = TurnOn_PB1_Wait;
+		tmpB = 0x01;
 	    } else if (!tmpA){
 		TurnOn_State = TurnOn_PB1;
 	    }
@@ -73,7 +75,7 @@ int main(void) {
     DDRA = 0x00; PORTA = 0xFF;
     DDRB = 0xFF; PORTB = 0x00;
 
-    TurnOn_State = TurnOn_Start;
+    TurnOn_State = TurnOn_PB0;
 
     /* Insert your solution below */
     while (1) {
